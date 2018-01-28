d043c47e951fa3de9e9814dce28f241cce55c98f
bawenna <wzhang87@hawk.iit.edu>
2018-01-28 13:12:16 -0600

    Added validation for phone & email

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,13 +1,28 @@
 $('#contact').on('submit',
   function(e) {
-    var emailPattern = ^.+@.+$;
+    var emailPattern = /^.+@.+$/;
     var emailVal = $('#email').val();
+    var phonePattern = /\d{3}[\-]\d{3}[\-]\d{4}/;
+    var phoneVal = $('#phone').val();
+    e.preventDefault();
+    console.log("Trying");
+    if(!(phoneVal.match(phonePattern)))
+    {
+      console.log("Trying");
+      alert('Please match the phone format of "xxx-xxx-xxxx"');
+      return false;
+    }
     if(!(emailVal.match(emailPattern)))
     {
+      console.log("Trying");
       alert('Please match the email format of "you@example.com"');
       return false;
     }
-    $('#contact').append('The form was submitted.');
-    e.preventDefault();
+
+    else
+    {
+    $('#contact').append('The form was submitted!');
+    document.getElementById('submit').style.visibility = 'hidden';
+    }
   }
 );
